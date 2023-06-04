f9eabb085ee24bb95aa5e2f2d2c23e454acdb5d8
hiroro <hiroaki.a1989@gmail.com>
2023-06-04 00:34:59 -0400

    added part of api file

--- models/index.js
+++ models/index.js
@@ -1,39 +1,35 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.belongsTo(Category,{
-  foreignKey: 'category_id',
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
 });
 
 // Categories have many Products
-Category.hasMany(Product,{
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
-})
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag,{
-  through:{
-    model: ProductTag,
-    unique: false
-  },
-  as : 'product_tag'
-
-})
-
-// Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product,{
+Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
-    unique: false
   },
-  as: 'tag_product'
+  // as: "product_tag",
+});
 
-})
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  // as: "tag_product",
+});
 
 module.exports = {
   Product,
@@ -41,3 +37,4 @@ module.exports = {
   Tag,
   ProductTag,
 };
+//
